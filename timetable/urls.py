4610d7f7407cb4cfee1c02e0d47112c0c088e755
Petrauskas <petra.gytis@gmail.com>
2022-04-18 23:12:22 +0300

    Changed base template, added index and data preparation 'backbone' templates;

--- timetable/urls.py
+++ timetable/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('preparation/', views.prepare_data, name='prepare_data'),
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 
+from .models import Group, Lecturer, CourseUnit, Student, Faculty, Classroom
+
 
 # Landing page
 def index(request):
     return render(request, 'index.html')
+
+
+# Data preparation for schedule generation page
+def prepare_data(request):
+    groups = Group.objects.all()
+    lecturers = Lecturer.objects.all()
+    course_units = CourseUnit.objects.all()
+    students = Student.objects.all()
+    faculties = Faculty.objects.all()
+    classrooms = Classroom.objects.all()
+
+    context = {
+        'groups': groups,
+        'lecturers': lecturers,
+        'course_units': course_units,
+        'students': students,
+        'faculties': faculties,
+        'classrooms': classrooms,
+    }
+    return render(request, 'data_preparation.html', context=context)
